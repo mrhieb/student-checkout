cd753473ed33b0acc25a1166e117f0f22146d14a
jerhieb <jeremy.hieb@moundsviewschools.org>
2019-11-08 22:50:03 -0600

    Adding name, time, and location to csv.

--- bathroom pass app.py
+++ bathroom pass app.py
@@ -3,21 +3,24 @@ import csv
 import time
 '''
 Next steps....
-Capture the time that the student leaves and returns.  Create a dictionary for leave times.
-Create a dictionary for return times.  Create a dictionary for time away
-Find the difference.  Put that number in a csv with the student's name.
+
 Have a space to label the students that are gone and the students that
 are waiting to leave the classroom.
 Limit the number of students leaving to 2.
 Properly clear the screen 3 seconds after they choose a location or report
-back to class.
+back to class.  If a student returns back to class, take their name off the gui.
+
 
 '''
 class_number = 0
 disk_number = 0
 combo = ''
 student_reason={}
-out_of_class=[]
+out_of_class1=[]
+out_of_class2 = []
+out_of_class3 = []
+out_of_class4 = []
+out_of_class5 = []
 reasons = []
 def reset_screen():
     combo.visible = False
@@ -30,7 +33,18 @@ def reset_screen():
     drink.visible = False
     nurse.visible = False
     other.visible = False
-    
+    hour1.bg = 'white'
+    hour2.bg = 'white'
+    hour3.bg = 'white'
+    hour4.bg = 'white'
+    hour5.bg = 'white'
+    hour6.bg = 'white'
+
+def record_data(student_list):
+    with open('StudentsOut.csv', 'a') as csv_file:
+        csv_appender = csv.writer(csv_file)
+        csv_appender.writerow(student_list)
+        
 def specific_names():
     global class_number
     global disk_number
@@ -42,7 +56,16 @@ def specific_names():
         for row in csv_reader:
             if line_count == class_number:
                 name_space.value =row[disk_number]
-                out_of_class.append(row[disk_number])
+                if len(out_of_class1)==0:
+                    out_of_class1.append(row[disk_number])
+                elif len(out_of_class2) == 0:
+                    out_of_class2.append(row[disk_number])
+                elif len(out_of_class3) == 0:
+                    out_of_class3.append(row[disk_number])
+                elif len(out_of_class4) == 0:
+                    out_of_class4.append(row[disk_number])
+                elif len(out_of_class2) == 0:
+                    out_of_class5.append(row[disk_number])
                 checkout_button.visible = True
                 checkin_button.visible = True
             line_count+=1
@@ -57,7 +80,7 @@ def first_hour():
     hour4.bg = 'white'
     hour5.bg = 'white'
     hour6.bg = 'white'
-    class_number = 1
+    class_number = 0
     hour1.bg = 'green'
     disk_number_text.value = 'What is your disk number?'
     combo.visible = True
@@ -79,7 +102,10 @@ def second_hour():
     disk_number_text.value = 'What is your disk number?'
     combo.visible = True
 def third_hour():
-    
+    global class_number
+    global disk_number
+    global combo
+    class_number = 2
     hour1.bg = 'white'
     hour2.bg = 'white'
     hour3.bg = 'white'
@@ -90,6 +116,10 @@ def third_hour():
     disk_number_text.value = 'What is your disk number?'
     combo.visible = True
 def fourth_hour():
+    global class_number
+    global disk_number
+    global combo
+    class_number = 3
     hour1.bg = 'white'
     hour2.bg = 'white'
     hour3.bg = 'white'
@@ -100,23 +130,31 @@ def fourth_hour():
     disk_number_text.value = 'What is your disk number?'
     combo.visible = True
 def fifth_hour():
+    global class_number
+    global disk_number
+    global combo
+    class_number = 4
     hour1.bg = 'white'
     hour2.bg = 'white'
     hour3.bg = 'white'
     hour4.bg = 'white'
     hour5.bg = 'white'
     hour6.bg = 'white'
-    hour3.bg = 'green'
+    hour5.bg = 'green'
     disk_number_text.value = 'What is your disk number?'
     combo.visible = True
 def sixth_hour():
+    global class_number
+    global disk_number
+    global combo
+    class_number = 5
     hour1.bg = 'white'
     hour2.bg = 'white'
     hour3.bg = 'white'
     hour4.bg = 'white'
     hour5.bg = 'white'
     hour6.bg = 'white'
-    hour3.bg = 'green'
+    hour6.bg = 'green'
     disk_number_text.value = 'What is your disk number?'
     combo.visible = True
 
@@ -127,25 +165,44 @@ def checkout_options():
     other.visible = True
 
 def welcome_back():
+    end_time = time.time()
+    total_time = end_time-out_of_class1[1]
+    out_of_class1.append(total_time)
     welcome_text.value = 'Welcome back to class!!!'
     welcome_text.text_size = 40
     welcome_text.text_color = 'white'
-
+    reset_screen()
+    record_data(out_of_class1)
+    
+    
 def going_bathroom():
-    Text(app, out_of_class[-1]+ ' to the bathroom')
+    start_time = time.time()
+    
+    out_of_class1.append(start_time)
+    out_of_class1.append('bathroom')
+    Text(app, out_of_class1[0]+ ' to the bathroom')
     reset_screen()
 
 
 def going_drink():
-    Text(app, 'Go get a drink')
+    start_time = time.time()
+    out_of_class1.append(start_time)
+    out_of_class1.append('drink')
+    Text(app, out_of_class1[0]+'Go get a drink')
     reset_screen()
 
 def going_nurse():
-    Text(app, 'Go to nurse')
+    start_time = time.time()
+    out_of_class1.append(start_time)
+    out_of_class1.append('nurse')
+    Text(app, out_of_class1[0]+'Go to nurse')
     reset_screen()
 
 def going_other():
-    Text(app, 'Go to other')
+    start_time = time.time()
+    out_of_class1.append(start_time)
+    out_of_class1.append('other')
+    Text(app, out_of_class1[0]+'Go to other')
     reset_screen()
     
 
